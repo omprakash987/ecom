24fa9217bd7bd0028f5dd9401f3f73dd42dc0342
omprakash parewa <kanuparewa371@gmail.com>
2026-02-27 19:38:27 +0530

    baseurl changed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,8 +3,8 @@
 import axios from 'axios'; 
 
 const axiosInstance = axios.create({
-    // baseURL:"https://ecom-ev1s.onrender.com/api",
-    baseURL: "http://localhost:5000/api",
+    baseURL:"https://ecom-ev1s.onrender.com/api",
+    // baseURL: "http://localhost:5000/api",
     withCredentials:true,
 })
 
