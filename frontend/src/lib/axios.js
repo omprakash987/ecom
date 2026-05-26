a84e7d43b26c4f31c41cd3f589788cd13232d02c
omprakash parewa <kanuparewa371@gmail.com>
2026-05-26 11:43:58 +0530

    added www in axios baseurl

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     // baseURL:"https://ecom-ev1s.onrender.com/api",
-baseURL:"https://muscleup24x7.com/api",
+baseURL:"https://www.muscleup24x7.com/api",
     // baseURL: "http://localhost:5000/api",
     withCredentials:true,
 })
